cfcd159d12b3414e907a0fd5ebe8ffc4e31222a0
18627659723@163.com <150>
2017-02-15 11:57:35 +0800

    from pc change

--- PythonL/gevent.py
+++ PythonL/gevent.py
@@ -5,6 +5,7 @@ def f(url):
     resp = urlopen(url)
     data = resp.read()
     print('%d bytes received from %s.' % (len(data), url))#here is a git test
+    #from the pc
 gevent.joinall([
         gevent.spawn('https://www.python.org/'),
         gevent.spawn('https://www.yahoo.com/'),
